6b09040bf8ef9ea9c0dc5c704f832c5182682cbc
Rajur <rajur.hint@gmail.com>
2015-10-20 12:45:02 +0100

    Löödud controllerid ja vaated lahku

---
+++ controllers/home.php
@@ -0,0 +1,8 @@
+<?
+
+//retrieve data from database
+$q = mysqli_query($db, "SELECT * FROM posts NATURAL JOIN authors");
+while ($row = mysqli_fetch_assoc($q)) {
+    $posts[] = $row;
+}
+
--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?
+// Set timezone
+date_default_timezone_get('Europe/Tallinn');
 
+//Save wall clock time at the start of the script
 $page_load_start = microtime(1);
 // Connect to db
 require 'include/database.php';
 
 // Set page
-$page = !empty($_GET['page']) && file_exists("pages/$_GET[page].php") ? $_GET['page'] : 'home';
-var_dump($page);
-//Include required page
+$page = !empty($_GET['page']) ? $_GET['page'] : 'home';
 
+
+// Include controller if it exists
+if (file_exists("controllers/$page.php")) {
+    require 'controllers/$page.php';
+
+}
+
+//Include template
 require "templates/master_template.php";
 
 // Show page
-
 echo "Lehte laaditi " . round(microtime(1) - $page_load_start, 4) . " sekundit";
